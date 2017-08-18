e68f934b125c4c1abe10b4d19b425f8343f47862
Marcus Svartmark <marcus.svartmark@redpill-linpro.com>
2017-08-18 14:40:13 +0200

    Fix transactional consistency on writes to the s3 storage

--- alfresco-s3-adapter-platform/src/main/java/org/redpill/alfresco/s3/S3ContentStore.java
+++ alfresco-s3-adapter-platform/src/main/java/org/redpill/alfresco/s3/S3ContentStore.java
@@ -33,184 +33,185 @@ import java.util.Collections;
 import java.util.GregorianCalendar;
 import java.util.Map;
 
-
 public class S3ContentStore extends AbstractContentStore
         implements ApplicationContextAware, ApplicationListener<ApplicationEvent> {
 
-    private static final Log logger = LogFactory.getLog(S3ContentStore.class);
-    private ApplicationContext applicationContext;
+  private static final Log logger = LogFactory.getLog(S3ContentStore.class);
+  private ApplicationContext applicationContext;
 
-    private AmazonS3 s3Client;
-    private TransferManager transferManager;
+  private AmazonS3 s3Client;
+  private TransferManager transferManager;
 
-    private String accessKey;
-    private String secretKey;
-    private String bucketName;
-    private String regionName;
-    private String rootDirectory;
+  private String accessKey;
+  private String secretKey;
+  private String bucketName;
+  private String regionName;
+  private String rootDirectory;
+  private String endpoint;
 
-    @Override
-    public boolean isWriteSupported() {
-        return true;
+  @Override
+  public boolean isWriteSupported() {
+    return true;
+  }
+
+  @Override
+  public ContentReader getReader(String contentUrl) {
+
+    String key = makeS3Key(contentUrl);
+    return new S3ContentReader(key, contentUrl, s3Client, bucketName);
+
+  }
+
+  public void init() {
+
+    AWSCredentials credentials = null;
+
+    if (StringUtils.isNotBlank(this.accessKey) && StringUtils.isNotBlank(this.secretKey)) {
+
+      logger.debug("Found credentials in properties file");
+      credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
+
+    } else {
+      try {
+        logger.debug("AWS Credentials not specified in properties, will fallback to credentials provider");
+        credentials = new ProfileCredentialsProvider().getCredentials();
+      } catch (Exception e) {
+        logger.error("Can not find AWS Credentials");
+      }
     }
 
-    @Override
-    public ContentReader getReader(String contentUrl) {
+    s3Client = new AmazonS3Client(credentials);
+    if (!"".equals(endpoint)) {
+      logger.debug("Using custom endpoint" + endpoint);
+      s3Client.setEndpoint(endpoint);
+    } else if (logger.isDebugEnabled()) {
+      Region region = Region.getRegion(Regions.fromName(this.regionName));
+      logger.debug("Using default Amazon S3 endpoint with region " + region);
+      s3Client.setRegion(region);
+    }
+    transferManager = new TransferManager(s3Client);
+  }
 
-        String key = makeS3Key(contentUrl);
-        return new S3ContentReader(key, contentUrl, s3Client, bucketName);
+  public void setAccessKey(String accessKey) {
+    this.accessKey = accessKey;
+  }
 
+  public void setSecretKey(String secretKey) {
+    this.secretKey = secretKey;
+  }
+
+  public void setBucketName(String bucketName) {
+    this.bucketName = bucketName;
+  }
+
+  public void setRegionName(String regionName) {
+    this.regionName = regionName;
+  }
+
+  public void setEndpoint(String endpoint) {
+    this.endpoint = endpoint;
+  }
+
+  public void setRootDirectory(String rootDirectory) {
+
+    String dir = rootDirectory;
+    if (dir.startsWith("/")) {
+      dir = dir.substring(1);
     }
 
-    public void init() {
+    this.rootDirectory = dir;
+  }
 
-        AWSCredentials credentials = null;
+  @Override
+  public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+    this.applicationContext = applicationContext;
+  }
 
-        if(StringUtils.isNotBlank(this.accessKey) && StringUtils.isNotBlank(this.secretKey)) {
+  @Override
+  protected ContentWriter getWriterInternal(ContentReader existingContentReader, String newContentUrl) {
 
-            logger.debug("Found credentials in properties file");
-            credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
+    String contentUrl = newContentUrl;
 
-        } else {
-            try {
-                logger.debug("AWS Credentials not specified in properties, will fallback to credentials provider");
-                credentials = new ProfileCredentialsProvider().getCredentials();
-            } catch (Exception e) {
-                logger.error("Can not find AWS Credentials");
-            }
-        }
-
-        s3Client = new AmazonS3Client(credentials);
-        Region region = Region.getRegion(Regions.fromName(this.regionName));
-        s3Client.setRegion(region);
-        transferManager = new TransferManager(s3Client);
+    if (StringUtils.isBlank(contentUrl)) {
+      contentUrl = createNewUrl();
     }
 
+    String key = makeS3Key(contentUrl);
 
-    public void setAccessKey(String accessKey) {
-        this.accessKey = accessKey;
+    return new S3ContentWriter(bucketName, key, contentUrl, existingContentReader, s3Client, transferManager);
+
+  }
+
+  public static String createNewUrl() {
+
+    Calendar calendar = new GregorianCalendar();
+    int year = calendar.get(Calendar.YEAR);
+    int month = calendar.get(Calendar.MONTH) + 1;  // 0-based
+    int day = calendar.get(Calendar.DAY_OF_MONTH);
+    int hour = calendar.get(Calendar.HOUR_OF_DAY);
+    int minute = calendar.get(Calendar.MINUTE);
+    // create the URL
+    StringBuilder sb = new StringBuilder(20);
+    sb.append(FileContentStore.STORE_PROTOCOL)
+            .append(ContentStore.PROTOCOL_DELIMITER)
+            .append(year).append('/')
+            .append(month).append('/')
+            .append(day).append('/')
+            .append(hour).append('/')
+            .append(minute).append('/')
+            .append(GUID.generate()).append(".bin");
+    String newContentUrl = sb.toString();
+    // done
+    return newContentUrl;
+
+  }
+
+  private String makeS3Key(String contentUrl) {
+    // take just the part after the protocol
+    Pair<String, String> urlParts = super.getContentUrlParts(contentUrl);
+    String protocol = urlParts.getFirst();
+    String relativePath = urlParts.getSecond();
+    // Check the protocol
+    if (!protocol.equals(FileContentStore.STORE_PROTOCOL)) {
+      throw new UnsupportedContentUrlException(this, protocol + PROTOCOL_DELIMITER + relativePath);
     }
 
-    public void setSecretKey(String secretKey) {
-        this.secretKey = secretKey;
+    return rootDirectory + "/" + relativePath;
+
+  }
+
+  @Override
+  public boolean delete(String contentUrl) {
+
+    try {
+      String key = makeS3Key(contentUrl);
+      logger.debug("Deleting object from S3 with url: " + contentUrl + ", key: " + key);
+      s3Client.deleteObject(bucketName, key);
+      return true;
+    } catch (Exception e) {
+      logger.error("Error deleting S3 Object", e);
     }
 
-    public void setBucketName(String bucketName) {
-        this.bucketName = bucketName;
+    return false;
+
+  }
+
+  /**
+   * Publishes an event to the application context that will notify any
+   * interested parties of the existence of this content store.
+   *
+   * @param context the application context
+   * @param extendedEventParams
+   */
+  private void publishEvent(ApplicationContext context, Map<String, Serializable> extendedEventParams) {
+    context.publishEvent(new ContentStoreCreatedEvent(this, extendedEventParams));
+  }
+
+  public void onApplicationEvent(ApplicationEvent event) {
+    // Once the context has been refreshed, we tell other interested beans about the existence of this content store
+    // (e.g. for monitoring purposes)
+    if (event instanceof ContextRefreshedEvent && event.getSource() == this.applicationContext) {
+      publishEvent(((ContextRefreshedEvent) event).getApplicationContext(), Collections.<String, Serializable>emptyMap());
     }
-
-    public void setRegionName(String regionName) {
-        this.regionName = regionName;
-    }
-
-    public void setRootDirectory(String rootDirectory) {
-
-        String dir = rootDirectory;
-        if (dir.startsWith("/")) {
-            dir = dir.substring(1);
-        }
-
-        this.rootDirectory = dir;
-    }
-
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        this.applicationContext = applicationContext;
-    }
-
-
-    @Override
-    protected ContentWriter getWriterInternal(ContentReader existingContentReader, String newContentUrl) {
-
-        String contentUrl = newContentUrl;
-
-        if(StringUtils.isBlank(contentUrl)) {
-            contentUrl = createNewUrl();
-        }
-
-        String key = makeS3Key(contentUrl);
-
-        return new S3ContentWriter(bucketName, key, contentUrl, existingContentReader, s3Client, transferManager);
-
-    }
-
-    public static String createNewUrl() {
-
-        Calendar calendar = new GregorianCalendar();
-        int year = calendar.get(Calendar.YEAR);
-        int month = calendar.get(Calendar.MONTH) + 1;  // 0-based
-        int day = calendar.get(Calendar.DAY_OF_MONTH);
-        int hour = calendar.get(Calendar.HOUR_OF_DAY);
-        int minute = calendar.get(Calendar.MINUTE);
-        // create the URL
-        StringBuilder sb = new StringBuilder(20);
-        sb.append(FileContentStore.STORE_PROTOCOL)
-                .append(ContentStore.PROTOCOL_DELIMITER)
-                .append(year).append('/')
-                .append(month).append('/')
-                .append(day).append('/')
-                .append(hour).append('/')
-                .append(minute).append('/')
-                .append(GUID.generate()).append(".bin");
-        String newContentUrl = sb.toString();
-        // done
-        return newContentUrl;
-
-    }
-
-    private String makeS3Key(String contentUrl)
-    {
-        // take just the part after the protocol
-        Pair<String, String> urlParts = super.getContentUrlParts(contentUrl);
-        String protocol = urlParts.getFirst();
-        String relativePath = urlParts.getSecond();
-        // Check the protocol
-        if (!protocol.equals(FileContentStore.STORE_PROTOCOL))
-        {
-            throw new UnsupportedContentUrlException(this, protocol + PROTOCOL_DELIMITER + relativePath);
-        }
-
-
-        return rootDirectory + "/" + relativePath;
-
-    }
-
-    @Override
-    public boolean delete(String contentUrl) {
-
-        try {
-            String key = makeS3Key(contentUrl);
-            logger.debug("Deleting object from S3 with url: " + contentUrl + ", key: " + key);
-            s3Client.deleteObject(bucketName, key);
-            return true;
-        } catch (Exception e) {
-            logger.error("Error deleting S3 Object", e);
-        }
-
-        return false;
-
-    }
-
-    /**
-     * Publishes an event to the application context that will notify any interested parties of the existence of this
-     * content store.
-     *
-     * @param context
-     *            the application context
-     * @param extendedEventParams
-     */
-    private void publishEvent(ApplicationContext context, Map<String, Serializable> extendedEventParams)
-    {
-        context.publishEvent(new ContentStoreCreatedEvent(this, extendedEventParams));
-    }
-
-    public void onApplicationEvent(ApplicationEvent event)
-    {
-        // Once the context has been refreshed, we tell other interested beans about the existence of this content store
-        // (e.g. for monitoring purposes)
-        if (event instanceof ContextRefreshedEvent && event.getSource() == this.applicationContext)
-        {
-            publishEvent(((ContextRefreshedEvent) event).getApplicationContext(), Collections.<String, Serializable> emptyMap());
-        }
-    }
+  }
 }
--- run.sh
+++ run.sh
@@ -3,5 +3,7 @@ if [[ -z ${MAVEN_OPTS} ]]; then
     echo "The environment variable 'MAVEN_OPTS' is not set, setting it for you";
     MAVEN_OPTS="-Xms256m -Xmx2G"
 fi
+
+#MAVEN_OPTS="$MAVEN_OPTS -Djavax.net.ssl.trustStore=./java_cacerts"
 echo "MAVEN_OPTS is set to '$MAVEN_OPTS'";
 mvn clean install alfresco:run
