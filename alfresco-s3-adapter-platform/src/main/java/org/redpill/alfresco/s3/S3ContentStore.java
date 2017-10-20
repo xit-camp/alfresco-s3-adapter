c53803d8cee97e53fc6d224b176ae2c3cd350404
Erik Billerby <billerby@gmail.com>
2017-10-20 09:05:19 +0200

    logging.

--- alfresco-s3-adapter-platform/src/main/java/org/redpill/alfresco/s3/S3ContentStore.java
+++ alfresco-s3-adapter-platform/src/main/java/org/redpill/alfresco/s3/S3ContentStore.java
@@ -140,7 +140,9 @@ public class S3ContentStore extends AbstractContentStore
       
     } else {
       try {
-        LOG.debug("AWS Credentials not specified in properties, will fallback to credentials provider");
+        if (LOG.isDebugEnabled()){
+          LOG.debug("AWS Credentials not specified in properties, will fallback to credentials provider");
+        }
         credentials = new ProfileCredentialsProvider().getCredentials();
       } catch (Exception e) {
         LOG.error("Can not find AWS Credentials. Trying anonymous.", e);
